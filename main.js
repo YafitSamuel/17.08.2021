0e5f5d86ef8769ba57bdc8e4361afc629090c0d8
YafitSamuel <gyafit642@gmail.com>
2021-08-18 07:50:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,193 @@
+
+// ==================================17.08.2021====================================
+// ==================================================1
+// function getString(name){
+//    if(name.length>5){
+//     return "long"
+//    }
+//    return "short"
+// }
+// console.log(getString("yafit1"));
+// ==================================================2
+// function getString(array){
+//     if(array.indexOf("a")!=-1){
+//         return array.indexOf ("a") 
+//     }
+//     return "not found"
+// }
+// console.log(getString("array"));
+// ==================================================3
+// function getString(bar){
+//     if(bar.indexOf("r")!=-1){
+//         return true
+//     }
+//     return "false"
+// }
+// console.log(getString("bar"));
+//  ==================================================4
+// function getStr(str,t){
+//     if(str.indexOf(t)!=-1){
+//         return true
+//     }
+//     return false
+// }
+// console.log( getStr(wer,r));
+//  ==================================================5
+//  
+
+
+
+//  ==================================================6
+// function getPrintSay(word) {
+//     return word.substr(0, 3)
+// }
+// console.log(getPrintSay("sayonara"));
+//  ==================================================7
+// function getSubstr() {
+//     return "sayonara".substr(2,4)
+// }
+// console.log(getSubstr("sayonara"));
+//  ==================================================8
+// function getSubStringo() {
+//         return "sayonara".substr(2)
+//     }
+//     console.log(getSubStringo("sayonara"));
+//  ==================================================9
+// function getStringAndIndex(mysring, index) {
+//     if (mysring.length < index) {
+//         return " big"
+//     }
+//     return mysring.substr(3)
+// }
+// console.log(getStringAndIndex("yafit", 2));
+//  ==================================================10
+// function myString2(mysring1,index){
+//     if(mysring1.length<index){
+//         return "big"
+//     }
+//     return mysring1.substr(1,3)
+// }
+// console.log(myString2("yairi",3));
+//  ==================================================11
+
+
+
+
+
+
+// ==================================================12
+// function threeTav(){
+//     return "sayonara".substring(0,3)
+// }
+// console.log(threeTav(""));
+//  ==================================================13
+// function tavs(){
+//     return "sayonara".substring(2,6)
+//  / }
+// console.log(tavs());
+//  ==================================================14
+// function gttCharOfo(str){
+//     return str.substring(3)
+// }
+// console.log(gttCharOfo("sayonary"));
+
+// //  ==================================================15
+// function getFullName(myname) {
+//     console.log(myname.length);
+//     if (myname.indexOf(" " != -1)){
+//         console.log("have apace");
+//     }
+// }
+// console.log(getFullName("yafitsumel"));
+
+       // *שם פרטי ושם משפחה בנפרד
+
+// // //  =================================================16
+// function getTwoSring(strin1, string2) {
+//     let result = string2 + string1
+//     if (string1.toLocaleLowerCase() == string2.toLocaleLowerCase()) {
+//         return result
+//     }
+// }
+// console.log(getTwoSring("yafit", "Yffit"));
+//   =====================================================17
+// function getTwoSring2(string1, string2) {
+//     let result = string2 + string1
+//     if (string1.toUpperCase() == string2.toUpperCase()) {
+//         return result
+//     }
+// }
+// console.log(getTwoSring2("yafit", "Yffit"));
+
+//   =====================================================18
+// function getFullName(firstName, lastName) {
+//     if (firstName.length > lastName.length) {
+//         console.log(firstName);
+//     } else {
+//         console.log(lastName);
+//     }
+// }
+// let firstName = prompt("please enter first name")
+// let lastName = prompt("please enter last name")
+// console.log(getFullName("firstName", "lastName"));
+//=========================================================19
+    // let userInputOne = prompt("enter a first name");
+    // let userInpuTwo = prompt("enter a last name");
+    //  function getNameFromUser (firstName, lastName){
+    //      if (firstName.indexOf("a")!=-1){
+    //          console.log(firstName);
+    //      }
+    //     if (lastName.indexOf("api")!=-1){
+    //              console.log(lastName);
+    //          }
+    //  }
+    //   getNameFromUser (userInputOne,userInpuTwo);
+
+// ========================================================20
+// function  getstring (str){
+//     if (str.length>5){
+//         return "long"
+//     }
+//     return "short"
+// }
+// console.log(getstring ("yafi"));
+// =========================================================21
+// function  getstringOfCountry (str){
+//     if (str.length<3){
+//         return "yes"
+//     }
+//     return "no"
+// }
+// console.log(getstringOfCountry ("israel"));
+
+// =========================================================22
+// function getStringOrChar (str , char){
+//     if(str.length>6){
+//         return str
+//     }
+//     return str.indexOf(char)
+// }
+// let result= getStringOrChar ("lior", "l");
+// console.log(result);
+// ==========================================================23
+
+
+
+
+
+
+// ==========================================================24
+// function getFullName (firstName, lastName ){
+//     return firstName.substr(0,1) + ". "  + lastName.toUpperCase()
+//   }
+//   console.log(getFullName ( "yafit", "samuel"));
+
+// ==========================================================25
+
+
+
+
+
+// ==========================================================26
+
+
